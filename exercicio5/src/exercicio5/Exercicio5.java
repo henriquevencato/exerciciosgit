ad726ac9bf51e476c51bcf5c8e7a5939252c2fe5
henrique vencato <henriquevencato2007@gmail.com>
2025-05-16 16:14:16 -0300

    exercicios 3,4 e 5

---
+++ exercicio5/src/exercicio5/Exercicio5.java
@@ -0,0 +1,25 @@
+
+package exercicio5;
+
+
+public class Exercicio5 {
+
+    
+        public static long fatorial(int n) {
+        if (n < 0) {
+            throw new IllegalArgumentException("Fatorial não é definido para números negativos.");
+        }
+
+        long resultado = 1;
+        for (int i = 2; i <= n; i++) {
+            resultado *= i;
+        }
+        return resultado;
+    }
+
+    public static void main(String[] args) {
+        int n1 = 5;
+        long resultado = fatorial(n1);
+        System.out.println("Fatorial de " + 1 + " é: " + resultado);
+    }
+}
